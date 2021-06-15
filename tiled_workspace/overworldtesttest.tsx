812fc2abdda2b4641fa841bf023532eebfbce27f
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-15 15:11:01 -0700

    3rd time trying to get prefabs working, sorry
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- tiled_workspace/overworldtesttest.tsx
+++ tiled_workspace/overworldtesttest.tsx
@@ -1,8 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
- <editorsettings>
-  <export target="overworldtesttest.tsx" format="tsx"/>
- </editorsettings>
  <image source="../../../../gameAssets/gfx/gfx/Overworld.png" width="640" height="576"/>
  <wangsets>
   <wangset name="path" type="mixed" tile="-1">
