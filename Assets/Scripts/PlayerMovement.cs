1c8aba54afc10921be2d70f1132fa952d8c03371
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-15 10:50:27 -0700

    player movement
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+   
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector2 change;
 
     // Start is called before the first frame update
-    void Start(){
+    void Start () {
         myRigidbody = GetComponet<Rigidbody2D>();     
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update () {
         change = Vector2.zero;
         change.x = Input.GetAxis("Horizontal");
         change.y = Input.GetAxis("Vertical");
