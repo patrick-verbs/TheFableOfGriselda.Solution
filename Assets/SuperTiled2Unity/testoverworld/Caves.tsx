be86a109ed79b22e30a8a47412e7e8b3c8687bd4
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-16 11:21:17 -0700

    patricks tile stitching
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

---
+++ Assets/SuperTiled2Unity/testoverworld/Caves.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Caves" tilewidth="16" tileheight="16" tilecount="9792" columns="102">
+ <image source="../../RPGW_Caves_v2/MainLev2.0.png" width="1632" height="1536"/>
+</tileset>
