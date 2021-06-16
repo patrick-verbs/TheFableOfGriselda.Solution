f766f2de3e0a3b819d886684220f16f8272991b6
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-16 10:52:52 -0700

    more art/map files
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

---
+++ Assets/SuperTiled2Unity/testoverworld/OverWorldRUSeth.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="OverWorldRUSeth" tilewidth="16" tileheight="16" tilecount="480" columns="16">
+ <image source="../../8BIT CanariPack TopDown/TILESET/PixelPackTOPDOWN8BIT.png" width="256" height="480"/>
+</tileset>
