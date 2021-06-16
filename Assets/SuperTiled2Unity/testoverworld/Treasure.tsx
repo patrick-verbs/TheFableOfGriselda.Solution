30c91332db1ec1ee554e361f1fc4ec62cd25c74e
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-16 12:02:23 -0700

    :tada:import seth maps
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- Assets/SuperTiled2Unity/testoverworld/Treasure.tsx
+++ Assets/SuperTiled2Unity/testoverworld/Treasure.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="Treasure" tilewidth="16" tileheight="16" tilecount="15" columns="3">
- <image source="../../chests_16x16_0.png" width="48" height="80"/>
+ <image source="chests_16x16_0.png" width="48" height="80"/>
 </tileset>
