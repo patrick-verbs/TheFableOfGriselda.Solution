0bf3b23c0d4d2531ac5f76a80ad2205fecaf5084
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-16 17:10:53 -0700

    :beers:Yall did it Thanks for the great week
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour {
    
@@ -8,15 +9,29 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    private bool isFox;
 
     // Start is called before the first frame update
     void Start () {
         animator = GetComponent<Animator>();
+        animator.SetBool("fox", false);
         myRigidbody = GetComponent<Rigidbody2D>();     
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update () 
+    { 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("fox", true);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("fox", false);
+        }
+
+
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");  
@@ -33,6 +48,14 @@ public class PlayerMovement : MonoBehaviour {
             animator.SetBool("moving", true);
         } else {
             animator.SetBool("moving", false);
+        }
+    }
+
+    void TransformCharacter()
+    {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            animator.SetBool("fox", true);
         }
     }
 
